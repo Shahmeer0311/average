9db0fe5df94a0031c992b546642bb936b35683f7
Shahmeer0311 <22alis@students.watfordboys.org>
2024-11-30 14:23:33 +0000

    Average

---
+++ Average.py
@@ -0,0 +1,9 @@
+Tree1 = int(input("What is the first Tree's height"))
+Tree2 = int(input("What is the second Tree's height"))
+Tree3 = int(input("What is the third Tree's height"))
+Tree4 = int(input("What is the fourth Tree's height"))
+Tree5 = int(input("What is the fifth Tree's height"))
+80
+average = (Tree1 + Tree2 + Tree3 + Tree4  +Tree5) // 5
+
+print(average)
